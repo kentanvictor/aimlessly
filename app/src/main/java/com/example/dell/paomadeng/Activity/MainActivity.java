15865ef8c7eca2300cd9c76ee95d7a8713b33d12
kentanvictor <2030457926@qq.com>
2017-05-05 20:41:38 +0800

    Use local broadcast

--- app/src/main/java/com/example/dell/paomadeng/Activity/MainActivity.java
+++ app/src/main/java/com/example/dell/paomadeng/Activity/MainActivity.java
@@ -7,6 +7,7 @@ import android.content.IntentFilter;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
@@ -16,23 +17,32 @@ import com.example.dell.paomadeng.R;
 
 public class MainActivity extends AppCompatActivity {
     private IntentFilter intentFilter;
+    private LocalReceiver localReceiver;
+    private LocalBroadcastManager localBroadcastManager;
     private NetworkChangeReceiver networkChangeReceiver;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        intentFilter = new IntentFilter();//IntentFilter是意图过滤器，一般定义在清单文件manifest.xml中
-        intentFilter.addAction("android.net.conn.CONNECTIVITY_CHANGE");
+        localBroadcastManager = LocalBroadcastManager.getInstance(this);//获取实例
         Button button = (Button) findViewById(R.id.but1);
         button.setOnClickListener(new View.OnClickListener()
         {
             @Override
             public void onClick(View v)
             {
+/*
                 Intent intent = new Intent("com.example.dell.paomadeng.BroadCastReceiver");
                 sendOrderedBroadcast(intent,null);
+*/
+                Intent intent = new Intent("com.example.dell.paomadeng.BroadCastReceiver");
+                localBroadcastManager.sendBroadcast(intent);//发送本地广播
             }
         });
+        intentFilter = new IntentFilter();//IntentFilter是意图过滤器，一般定义在清单文件manifest.xml中
+        intentFilter.addAction("com.example.dell.paomadeng.BroadCastReceiver");
+        localReceiver = new LocalReceiver();
+        localBroadcastManager.registerReceiver(localReceiver,intentFilter);//注册本地广播监听器
         /*
         * 广播接收器通过过滤器中的action、data和category来筛选广播
         * 上面添加的android.net.conn.CONNECTIVITY_CHANGE是一个action
@@ -41,19 +51,27 @@ public class MainActivity extends AppCompatActivity {
         * 调用registereReceiver()方法进行注册，将NetworkChangeReceiver的实例和IntentFilter的实例传进去
         * 这样NetworkChangeReceiver就会收到所有值为android.net.conn.CONNECTIVITY_CHANGE的广播，也就实现了监听网络变化的功能
         */
-        networkChangeReceiver = new NetworkChangeReceiver();
+        /*networkChangeReceiver = new NetworkChangeReceiver();
         registerReceiver(networkChangeReceiver,intentFilter);
-
+*/
 
     }
     @Override
     protected void onDestroy()
     {
         super.onDestroy();
-        unregisterReceiver(networkChangeReceiver);
+        /*unregisterReceiver(networkChangeReceiver);*/
+
     }
 
 
+    class LocalReceiver extends BroadcastReceiver{
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            Toast.makeText(context,"received local broadcast",Toast.LENGTH_SHORT).show();
+        }
+    }
 }
 class NetworkChangeReceiver extends BroadcastReceiver{//广播接收器，动态注册
     @Override
